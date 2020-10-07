8c2b2e0c105d3092ac1e8551e070b28f4e6f8eb5
Alden Ho <aldenho@Aldens-MacBook-Air.local>
2020-10-07 13:21:02 -0700

    pull request

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
+import axios from 'axios'
+
 function App() {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    // const fetchFriends = () => {
+      axios.get('https://api.nasa.gov/planetary/apod?api_key=S1uJsFVGc3nmjwnYYmeDyBsT71z766uujo3H1pQe')
+      .then(res => {
+        setData(res.data)
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    // fetchFriends()
+  }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>NASA Photo of the Day</h1>
+      <p>Welcome to the cosmos! Each day we'll show you a different image of our beautiful universe with a description written by a professional astronomer!</p>
+      <p></p>  
+      <div>
+      </div>
     </div>
   );
 }
